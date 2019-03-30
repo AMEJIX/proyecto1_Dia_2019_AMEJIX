7ce72acb3db3ccb8839be3d28934808447ad8ea6
jcambronerof <jcambronerof@ucenfotec.ac.cr>
2019-03-29 22:57:36 -0600

    Actualización 2.0

--- public/js/controlador.mostrarNoticias.js
+++ public/js/controlador.mostrarNoticias.js
@@ -5,7 +5,7 @@ let user = JSON.parse(sessionStorage.getItem("usuario"));
 
 let idUsuarioCE = user._id;
 
-if(user.userType != "centroEducativo") {
+if(user.userType != "centroEducativo" || typeof listaPreguntasFrecuentes == 'string') {
     idUsuarioCE = IdGeneralCE;
 } else {
 
