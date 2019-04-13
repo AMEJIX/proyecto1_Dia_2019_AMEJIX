96c92887f45dc2ed6c4debf5734a1e6a87c5598f
jcambronerof <jcambronerof@ucenfotec.ac.cr>
2019-04-13 12:53:21 -0500

    Casos de uso de Jimmy, segunda iteración versiones mejoradas

--- public/js/controlador.profileInfoCE.js
+++ public/js/controlador.profileInfoCE.js
@@ -3,14 +3,14 @@
 let usuarioLogeado = JSON.parse(sessionStorage.getItem("usuario"));
 
 'use strict';
-const aInfoCE= document.querySelector('#aInfoCE');
-const aMatricula= document.querySelector('#aMatricula');
-const aBecas= document.querySelector('#aBecas');
-const aCitas= document.querySelector('#aAgendarCita');
-const aMaterialInformativo= document.querySelector('#aMaterialInformativo');
-const aNoticias= document.querySelector('#aNoticias');
-const aActividades= document.querySelector('#aActividades'); 
-const aUtiles= document.querySelector('#aUtiles');
+const aInfoCE = document.querySelector('#aInfoCE');
+const aMatricula = document.querySelector('#aMatricula');
+const aBecas = document.querySelector('#aBecas');
+const aCitas = document.querySelector('#aAgendarCita');
+const aMaterialInformativo = document.querySelector('#aMaterialInformativo');
+const aNoticias = document.querySelector('#aNoticias');
+const aActividades = document.querySelector('#aActividades');
+const aUtiles = document.querySelector('#aUtiles');
 const aPreguntasFrecuentes = document.querySelector('#aPreguntasFrecuentes');
 // const = document.querySelector('#aEtiquetas');                          
 const volver = document.querySelector('#volver');
@@ -21,20 +21,21 @@ const volver = document.querySelector('#volver');
 
 let agregarVariosParametroUrl = (pParametro, ...args) => {
     let masParametros = '';
-    let i =0;
-    for(let arg of args){
+    let i = 0;
+    for (let arg of args) {
         if (i === 0) masParametros += '&centroEducativo=' + arg;
         i++;
     }
     aCitas.href = 'citas.html?idCE=' + pParametro + masParametros;
-    aInfoCE.href = 'informacionPersonalCEVistaOtroUsuario.html?idCE=' + pParametro+ masParametros;
-    aMatricula.href = 'mostrarMatriculaCostos.html?idCE=' + pParametro+ masParametros;
-    aBecas.href = 'mostrarBecas.html?idCE=' + pParametro+ masParametros;
-    aMaterialInformativo.href = 'mostrarMaterialInformativo.html?idCE=' + pParametro+ masParametros;
-    aNoticias.href = 'mostrarNoticias.html?idCE=' + pParametro+ masParametros;
-    aActividades.href ='listarActividades.html?idCE=' + pParametro+ masParametros;
-    aUtiles.href ='listarUtiles.html?idCE=' + pParametro+ masParametros;
-    aPreguntasFrecuentes.href = 'preguntasFrecuentesPF.html?idCE=' + pParametro+ masParametros;
+    aInfoCE.href = 'informacionPersonalCEVistaOtroUsuario.html?idCE=' + pParametro + masParametros;
+    aMatricula.href = 'mostrarMatriculaCostos.html?idCE=' + pParametro + masParametros;
+    aBecas.href = 'mostrarBecas.html?idCE=' + pParametro + masParametros;
+    aIdiomas.href = 'mostrarIdiomas.html?idCE=' + pParametro + masParametros;
+    aMaterialInformativo.href = 'mostrarMaterialInformativo.html?idCE=' + pParametro + masParametros;
+    aNoticias.href = 'mostrarNoticias.html?idCE=' + pParametro + masParametros;
+    aActividades.href = 'listarActividades.html?idCE=' + pParametro + masParametros;
+    aUtiles.href = 'listarUtiles.html?idCE=' + pParametro + masParametros;
+    aPreguntasFrecuentes.href = 'preguntasFrecuentesPF.html?idCE=' + pParametro + masParametros;
 };
 
 // agregarParametroUrl(IdGeneralCE);
@@ -45,11 +46,11 @@ function ocultar() {
     if (usuarioLogeado.userType == "padreFamilia") {
         document.querySelector('#menuCitasEnCE').style.display = 'block';
 
-    }else if(usuarioLogeado.userType == "superAdministrador"){
+    } else if (usuarioLogeado.userType == "superAdministrador") {
         document.querySelector('#menuCitasEnCE').style.display = 'none';
         document.querySelector('#aCitas').style.display = 'none';
-        
-    } else{
+
+    } else {
         volver.style.display = 'none';
     }
 }
