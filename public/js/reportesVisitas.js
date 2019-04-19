0b7663a0caac415513f66ad70976e07375221df4
asolanoruc <asolanor@ucenfotec.ac.cr>
2019-04-18 18:47:03 -0600

    actualizacion de reportes de visitas standard

--- public/js/reportesVisitas.js
+++ public/js/reportesVisitas.js
@@ -1,22 +1,31 @@
+'use strict';
+
+if (user.userType === 'padreFamilia') {
+    window.location = 'loSentimos.html'   
+}
+
+let prueba = 5;
+
 new Morris.Line({
     // ID of the element in which to draw the chart.
     element: 'myfirstchart',
     // Chart data records -- each entry in this array corresponds to a point on
     // the chart.
+    
     data: [
-      { year: '2008', value: 20 },
-      { year: '2009', value: 10 },
-      { year: '2010', value: 5 },
-      { year: '2011', value: 5 },
-      { year: '2012', value: 20 }
+      { year: '2008', Visitas: prueba },
+      { year: '2009', Visitas: prueba },
+      { year: '2010', Visitas: prueba },
+      { year: '2011', Visitas: prueba},
+      { year: '2012', Visitas: prueba }
     ],
     // The name of the data record attribute that contains x-values.
     xkey: 'year',
     // A list of names of data record attributes that contain y-values.
-    ykeys: ['value'],
+    ykeys: ['Visitas'],
     // Labels for the ykeys -- will be displayed when you hover over the
     // chart.
-    labels: ['Value'],
+    labels: ['Visitas'],
 
     resize: true,
     lineWidth: 2,
