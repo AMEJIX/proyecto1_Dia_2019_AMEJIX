3a88fe4a4d62a4ca037f27e3ac58b6a353bd34b3
asolanoruc <asolanor@ucenfotec.ac.cr>
2019-04-20 14:21:42 -0600

    avance de graficos

--- public/js/reportesVisitas.js
+++ public/js/reportesVisitas.js
@@ -8,32 +8,128 @@ if (user.userType === 'padreFamilia') {
   idUsuario = IdGeneralCE;
 }
 
-let visitas = obtenerVisita(user._id);
-let fechasVisitas = visitas.fechas;
+// let visitas = obtenerVisita(user._id);
+// let fechasVisitasUMT = visitas.fechas;
 
-console.log(fechasVisitas);
 
-new Morris.Line({
-  // ID of the element in which to draw the chart.
-  element: 'myfirstchart',
-  // Chart data records -- each entry in this array corresponds to a point on
-  // the chart.
+// function datos (){
+//   for (let i = 0; i < visitas.length; i++) {
+    
+//   }
+// };
 
-  data: [
-    { year: '2009', Visitas: 1 },
-    { year: '2010', Visitas: 1 },
-    { year: '2011', Visitas: 1 },
-    { year: '2012', Visitas: 1 }
-  ],
-  // The name of the data record attribute that contains x-values.
-  xkey: 'year',
-  // A list of names of data record attributes that contain y-values.
-  ykeys: ['Visitas'],
-  // Labels for the ykeys -- will be displayed when you hover over the
-  // chart.
-  labels: ['Visitas'],
+// console.log('visitas.fechas: ' + visitas.fechas);
+// console.log('FechasUMT: ' + fechasVisitasUMT);
+// console.log('Fechas UTC: ' + fechasVisitas);
 
-  resize: true,
-  lineWidth: 2,
-  lineColors: ['#F9AA33']
-});
+// new Morris.Line({
+
+//   element: 'myfirstchart',
+
+
+//   data: [
+//     { Fecha: '12 de Abril de 2019', Visitas: visitas.fechas[0] },
+//     { Fecha: '13 de Abril de 2019', Visitas: visitas.fechas[1] },
+//     { Fecha: '14 de Abril de 2019', Visitas: visitas.fechas[2] },
+//     { Fecha: '15 de Abril de 2019', Visitas: visitas.fechas[20] }
+//   ],
+
+//   xkey: 'Fecha',
+
+//   ykeys: ['Visitas'],
+
+//   labels: ['Visitas'],
+
+//   resize: true,
+//   lineWidth: 2,
+//   lineColors: ['#F9AA33']
+// });
+
+'use strict';
+
+// let user = JSON.parse(sessionStorage.getItem("usuario"));
+
+const tabla = document.querySelector('#tblStandard tbody');
+const inputFiltro = document.querySelector('#txtFiltro');
+
+let visitas = [];
+
+let usuarios = listarUsuariosCE();
+
+let visitasUsuarios = [];
+
+let esIndefinido = (variable) =>{
+    return  variable === undefined || variable === null;
+};
+
+let agregarVisitas = () =>{
+    let visitasUsuario;
+
+    for (let i = 0; i < usuarios.length; i++){
+        visitasUsuario = obtenerVisita(usuarios[i]._id);
+        if (visitasUsuario === false) {
+            visitasUsuario = Object.assign({}, {
+                visitas: {idCE: usuarios[i]['_id'], fechas: []}
+            });
+            visitasUsuarios.push(visitasUsuario.visitas.fechas);
+        } else {
+            visitasUsuarios.push(visitasUsuario.fechas);
+        }
+
+        usuarios[i] = Object.assign(usuarios[i],  {visitas: visitasUsuario});
+        // console.log(visitasUsuarios[i].length);
+    }
+    // console.log(visitasUsuarios);
+};
+
+agregarVisitas();
+
+mostrarDatos();
+
+
+
+inputFiltro.addEventListener('keyup', mostrarDatos);
+
+function mostrarDatos() {
+
+
+    let filtro = inputFiltro.value;
+    tabla.innerHTML = '';
+    console.log(visitasUsuarios.length);
+    for (let i = 0; i < usuarios.length; i++) {
+
+        // console.log(usuarios[i].visitas);
+        // let tmpVisitas = visitasUsuarios[i].length;
+        // console.log(tmpVisitas);
+
+
+        if (usuarios[i]['centroEducativo'].toLowerCase().includes(filtro.toLowerCase())
+        ) {
+
+            let fila = tabla.insertRow();
+
+            // let cantidadVisitas;
+            // if (esIndefinido(visitaCE)) cantidadVisitas = visitaCE.fechas.length;
+            // else cantidadVisitas = 0;
+            //
+            // console.log(cantidadVisitas);
+            let nombre = fila.insertCell();
+            // nombre.textContent = usuarios[i]['centroEducativo'];
+            let anio = fila.insertCell();
+            anio.textContent = '2019';
+            let mes = fila.insertCell();
+            mes.textContent = 'abril';
+            let visitas = fila.insertCell();
+            visitas.textContent = visitasUsuarios[i].length;
+
+
+            let cEElementa = document.createElement('a');
+            cEElementa.innerHTML= usuarios[i] ['centroEducativo'];
+            cEElementa.href =
+                'profileInfoCE.html?idCE=' + usuarios [i] ['_id'] + '&centroEducativo=' + usuarios [i] ['centroEducativo'];
+            cEElementa.value =  usuarios [i] ['_id'];
+
+            nombre.appendChild(cEElementa);
+        }
+    }
+}
