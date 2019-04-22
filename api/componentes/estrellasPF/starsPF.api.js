1f2935dc6cce19d245b13c958f2e6eecb9e0e9cb
asolanoruc <asolanor@ucenfotec.ac.cr>
2019-04-22 16:27:24 -0600

    Reporte del ranking por PF para CE

--- api/componentes/estrellasPF/starsPF.api.js
+++ api/componentes/estrellasPF/starsPF.api.js
@@ -82,9 +82,37 @@ module.exports.listarEvaluacion = (req, res) => {
             } else {
                 res.json({
                     success: false,
-                    stars: 'No se encontraron evaluaciones registradas'
+                    stars: 'No se encontraron evaluaciones registradas.'
                 })
             }
         }
     )
+};
+
+module.exports.listarEvaluacionUsuario = (req, res) => {
+    starsPFModel.find().then(
+        function (stars) {
+
+            let listaStarsUser = [];
+
+            for (let starsUser of stars) {
+                if (starsUser.idCE == req.body.idCE) {
+                    listaStarsUser.push(starsUser);
+                }
+            }
+            console.log(listaStarsUser);
+            console.log(req.body.idCE);
+            if (listaStarsUser.length > 0) {
+                res.json({
+                    success: true,
+                    stars: listaStarsUser
+                });
+            } else {
+                res.json({
+                    success: false,
+                    stars: 'No se encontraron evaluaciones para este centro eduacativo.'
+                });
+            }
+        }
+    );
 };
